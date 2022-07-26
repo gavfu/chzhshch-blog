41132202f94a650320c606869dbc224404cd3fec
gavfu <gavfu@outlook.com>
2022-07-26 23:41:23 +0800

    Add more economics blogs

--- sidebars/economics.js
+++ sidebars/economics.js
@@ -36,6 +36,66 @@ exports.sidebar = [
   },
   {
     type: 'doc',
+    id: 'economics/wolves',
+    label: '群狼争肉----国有股流通与国有资产蚕食、瓜分游戏！ (2006/3/10 0:11:53)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/20060310',
+    label: '在与美圆的货币战争中，中国正越来越面对着战败的危险 (2006/3/10 0:17:18)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/july7th',
+    label: '中国货币战争中的“七七”事变！ (2006/3/10 0:47:02)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/wen',
+    label: '温家宝先生发布会的最后一句话值得商榷！ (2006/3/14 21:57:34)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/greenism',
+    label: '环保主义，资本全球化意识形态制造的彻头彻尾谎言！ (2006/3/21 21:47:03)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/ussenator',
+    label: '美国的政协委员，凭什么在中国晃荡？ (2006/3/26 21:36:04)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/resource',
+    label: '资源消耗瓶颈，中国威胁论的新谎言。 (2006/3/27 13:54:02)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/realestatemarket',
+    label: '按中国目前的经济管理水平，楼市必然股市化！ (2006/3/29 11:01:52)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/taiwan1',
+    label: '“统一台湾”，糊涂蛋闹的混蛋假命题！ (2006/4/7 17:13:58)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/taiwan2',
+    label: '必须成立“台海戡乱办公室”督办台湾剿匪事宜！ (2006/4/8 17:37:50)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/taiwan3',
+    label: '在台港澳问题上，毛泽东等是有过失的！ (2006/4/9 17:24:45)'
+  },
+  {
+    type: 'doc',
+    id: 'economics/bush',
+    label: '布什，毛泽东的真正信徒！ (2006/4/9 22:49:42)'
+  },
+  {
+    type: 'doc',
     id: 'economics/stockschat',
     label: '股市闲谈：G股是G点，大牛不用套！ (2006/5/12 19:02:25)'
   },
