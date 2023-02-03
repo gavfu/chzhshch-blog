f1e8eba0459151afc480b532fc9a280f96eddf66
gavfu <gavfu@outlook.com>
2023-02-03 23:41:21 +0800

    Add swizzling DocItemLayout component

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -7,6 +7,20 @@ import styles from './styles.module.css';
 const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
 const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
 
+const rawHTML = `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5686832782200002"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-5686832782200002"
+     data-ad-slot="9479333372"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+`;
+
+
 /**
  * Created via command (https://docusaurus.io/docs/swizzling):
  * 
@@ -20,9 +34,8 @@ export default function TOC({className, ...props}) {
         linkClassName={LINK_CLASS_NAME}
         linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
       />
-      <div className={'table-of-contents'}>
-
-      </div>
+      {/* <div dangerouslySetInnerHTML={{ __html: rawHTML }}>
+      </div> */}
     </div>
   );
 }
